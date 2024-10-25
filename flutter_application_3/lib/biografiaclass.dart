30cb6ee8d8f30b0e6ffe6c57bff35fa5865b30a5
gabriele-sousa <gabriele.sousa14@etec.sp.gov.br>
2024-10-25 11:17:12 -0300

    classe biografia

---
+++ flutter_application_3/lib/biografiaclass.dart
@@ -0,0 +1,13 @@
+class Biografia {
+  final String titulo;
+  final String nome;
+  final String idade;
+  final String descricao;
+
+  const Biografia(
+    this.titulo,
+    this.nome,
+    this.idade,
+    this.descricao
+  );
+}
