573a6ed19b691197fdd4ad9ef450f91f1a3c0ca7
gabriele-sousa <gabriele.sousa14@etec.sp.gov.br>
2024-10-31 22:02:59 -0300

    atl

--- flutter_application_3/lib/main.dart
+++ flutter_application_3/lib/main.dart
@@ -1,20 +1,36 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello!'),
+        body: Stack(
+          children: [
+            // Imagem de fundo vinda da internet
+            Positioned.fill(
+              child: Image(
+              image: NetworkImage(
+                "https://bdjogos.com.br/capas/1429-doom-3-pc-capa-1.jpg", // URL da imagem
+              ),
+              ),
+            ),
+            // Conteúdo sobreposto
+            Center(
+              child: Text(
+                "Texto sobre o fundo",
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
